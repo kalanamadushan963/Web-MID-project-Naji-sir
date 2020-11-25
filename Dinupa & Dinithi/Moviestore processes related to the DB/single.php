36c4afc3e206a315c080c0f17a0126dd0914338d
Dinithi Perera <72876387+dinithiperera236@users.noreply.github.com>
2020-11-25 21:45:50 +0530

    Add files via upload

---
+++ Dinupa & Dinithi/Moviestore processes related to the DB/single.php
@@ -0,0 +1,32 @@
+<?php
+
+include "db_connect_ticket.php";
+
+$sql = "SELECT * FROM move WHERE movieID = '6' ";
+$result = $conn->query($sql);
+
+	while($row = $result->fetch_assoc())
+	{
+		
+	?>
+	
+	
+	<title><?php echo $row['movieTitle']; ?></title>
+	<style>
+	background-image: url("<?php echo $row['movieImg']; ?>");
+	</style>
+	
+	<body>
+
+<div class="bg-img">
+  <div class="container header">
+    <img src="image/logo.png" class="logo">
+   </div>
+    
+    <a href="index.php">
+        <button class="btn2"><i class="fa fa-close"></i></button>
+        </a>
+    
+    
+	
+	<?php } ?>
