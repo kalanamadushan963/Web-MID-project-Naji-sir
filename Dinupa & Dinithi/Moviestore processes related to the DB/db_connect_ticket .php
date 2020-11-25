28197ca042d28d15d7c80284d55efb08b8d817a2
Dinithi Perera <72876387+dinithiperera236@users.noreply.github.com>
2020-11-26 01:33:25 +0530

    Update db_connect_ticket .php

--- Dinupa & Dinithi/Moviestore processes related to the DB/db_connect_ticket .php
+++ Dinupa & Dinithi/Moviestore processes related to the DB/db_connect_ticket .php
@@ -11,6 +11,11 @@ $database_in_use = "book_ticket";
 //create a database connection instante 
 
 $conn = new mysqli($host, $username, $user_pass, $database_in_use);
+// for check the connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+} 
+echo "Connected successfully";
 
 
 
