201af41ec8f4dd4b179c2a1043a99d80f77bf57d
Dinupa Pathmila <64583812+Dinupapathmila53@users.noreply.github.com>
2020-11-25 22:25:56 +0530

    Add files via upload

---
+++ Dinupa & Dinithi/Admin panel processes related to the DB/logging.php
@@ -0,0 +1,31 @@
+<?php
+
+$host="localhost";
+$user="root";
+$password="usbw";
+$db="book_ticket";
+
+mysql_connect($host,$user,$password);
+mysql_select_db($db);
+
+if(isset($_POST['full_name_1'])){
+    $full_name_1=$_POST['full_name_1'];
+	$password_1=$_POST['password_1'];
+	
+	$sql="select * from admin_table where name='".$full_name_1."' AND password='".$password_1."' limit 1 ";
+
+	$result=mysql_query($sql);
+	
+	if(mysql_num_rows($result)==1){
+	    echo "You Have Successfully Logged in";
+	    exit();
+	}
+	else{
+	    echo "Incorrect Username or Password !";
+		exit();
+	}
+}
+
+?>
+
+
